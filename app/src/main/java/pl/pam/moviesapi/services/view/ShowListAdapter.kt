b10ba50e9686730163c28dcec0623bc9f7ec0167
Adam-Czerwinski <adam.czerwinski097@gmail.com>
2020-02-12 23:23:10 +0100

    revert swipe

--- app/src/main/java/pl/pam/moviesapi/services/view/ShowListAdapter.kt
+++ app/src/main/java/pl/pam/moviesapi/services/view/ShowListAdapter.kt
@@ -55,6 +55,11 @@ class ShowListAdapter(private val showList: MutableList<Show>, private val fragm
                 .into(holder.recyclerviewMovieBinding.showPoster)
 
 
+
+
+
+
+
         holder.recyclerviewMovieBinding.layoutShow.setOnLongClickListener {
 
             println("łapię")
@@ -64,111 +69,11 @@ class ShowListAdapter(private val showList: MutableList<Show>, private val fragm
             true
         };
 
-
-        holder.recyclerviewMovieBinding.layoutShow.setOnTouchListener(object :
-            OnSwipeTouchListener(holder.recyclerviewMovieBinding.layoutShow.context) {
-
-            override fun onSwipeTop() {
-                Log.i(ShowListAdapter::class.java.simpleName,"onSwipeTop")
-
-                super.onSwipeTop()
-            }
-
-            override fun onSwipeBottom() {
-                Log.i(ShowListAdapter::class.java.simpleName,"onSwipeBottom")
-                super.onSwipeBottom()
-            }
-
-            override fun onSwipeLeft() {
-                Log.i(ShowListAdapter::class.java.simpleName,"onSwipeLeft")
-                fragment.prevPage.callOnClick();
-                super.onSwipeLeft()
-            }
-
-            override fun onSwipeRight() {
-                Log.i(ShowListAdapter::class.java.simpleName,"onSwipeRight")
-                fragment.nextPage.callOnClick();
-                super.onSwipeRight()
-            }
-        })
-
     }
 
     inner class MoviesViewHolder(
         val recyclerviewMovieBinding: ShowBinding
     ) : RecyclerView.ViewHolder(recyclerviewMovieBinding.root)
-
-
-    open class OnSwipeTouchListener(ctx: Context) : OnTouchListener {
-
-        private val gestureDetector: GestureDetector
-
-        companion object {
-
-            private val SWIPE_THRESHOLD = 100
-            private val SWIPE_VELOCITY_THRESHOLD = 100
-        }
-
-        init {
-            gestureDetector = GestureDetector(ctx, GestureListener())
-        }
-
-        override fun onTouch(v: View, event: MotionEvent): Boolean {
-            return gestureDetector.onTouchEvent(event)
-        }
-
-        private inner class GestureListener : SimpleOnGestureListener() {
-
-
-            override fun onDown(e: MotionEvent): Boolean {
-                return true
-            }
-
-            override fun onFling(
-                e1: MotionEvent,
-                e2: MotionEvent,
-                velocityX: Float,
-                velocityY: Float
-            ): Boolean {
-                var result = false
-                try {
-                    val diffY = e2.y - e1.y
-                    val diffX = e2.x - e1.x
-                    if (Math.abs(diffX) > Math.abs(diffY)) {
-                        if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
-                            if (diffX > 0) {
-                                onSwipeRight()
-                            } else {
-                                onSwipeLeft()
-                            }
-                            result = true
-                        }
-                    } else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
-                        if (diffY > 0) {
-                            onSwipeBottom()
-                        } else {
-                            onSwipeTop()
-                        }
-                        result = true
-                    }
-                } catch (exception: Exception) {
-                    exception.printStackTrace()
-                }
-
-                return result
-            }
-
-
-        }
-
-        open fun onSwipeRight() {}
-
-        open fun onSwipeLeft() {}
-
-        open fun onSwipeTop() {}
-
-        open fun onSwipeBottom() {}
-    }
 
 
 }
